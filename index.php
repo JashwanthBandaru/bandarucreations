2fa9234ccc9b5c53ad8f7ae6df824e49a63d5fbd
JashwanthBandaru <bandaru.jashwanth7@gmail.com>
2021-07-09 18:25:05 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Bandaru Creations</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="css/bcreations.css" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700;900&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <div><h1>Bandaru Creations</h1></div>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
