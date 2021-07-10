65e429d3bbe77af8387b3dee6b153e1db1fefc14
JashwanthBandaru <bandaru.jashwanth7@gmail.com>
2021-07-10 15:17:21 +0530

    reponsive,navbar,footer

---
+++ nav.php
@@ -0,0 +1,34 @@
+<!-- Navigation -->
+<nav class="navbar navbar-default" role="navigation">
+    <div class="container " >
+        <!-- Below part is visible after navigation bar collapse -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->
+            <a class="navbar-brand" href="index.html"></a>
+        </div>
+
+
+        <div class="collapse navbar-collapse text-right " id="bs-example-navbar-collapse-1" >
+            <ul class="text-center nav navbar-nav center1" >
+                <li>
+                    <a href="index.html">Home</a>
+                </li>
+                <li>
+                    <a href="about.html">About</a>
+                </li>
+                <li>
+                    <a href="blog.html">contact</a>
+                </li>
+                <li>
+                    <a href="contact.php">feedback</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
